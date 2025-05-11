faeff8d9d32fed723350c87300ad3c9320264f4b
y1wtt <awataki1206@gmail.com>
2025-05-11 16:13:29 +0900

    feature: change get ip endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,11 @@
+mod current_ip;
 use reqwest::header::HeaderValue;
 use serde::{Deserialize, Serialize};
 
 const ENV_API_KEY: &str = "CLOUDFLARE_API_KEY";
-const DNS_ENDPOINT: &str = "https://1.1.1.1/dns-query";
+const DNS_ENDPOINT: &str = " https://cloudflare.com/cdn-cgi/trace";
 
 
-#[derive(Serialize, Deserialize, Debug)]
-#[allow(non_snake_case)]
-struct DNSAnswer{
-    name: String,
-    r#type: i8,
-    TTL: i8,
-    data: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[allow(non_snake_case)]
-struct DNSResp {
-    Answer: Vec<DNSAnswer>
-}
-async fn get_ip_from_cloudflare() -> Result<DNSResp, Box<dyn std::error::Error>> {
-    let query = [
-        ("name","whoami.cloudflare.com"),
-        ("type","TXT")
-    ];
-
-    let uri = reqwest::Url::parse_with_params(DNS_ENDPOINT, query)?;
-    let client = reqwest::Client::new();
-    let resp = client.get(uri).header("accept", HeaderValue::from_static("application/dns-json")).send().await?;
-    let body = resp.text().await?;
-
-    let serialized:DNSResp = serde_json::from_str(&body)?;
-    Ok(serialized)
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
@@ -45,16 +18,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
             panic!("Fail to get {} :{}", ENV_API_KEY, e);
         }
     };
-    let a = get_ip_from_cloudflare().await;
+    let a = current_ip::get_ip_from_cloudflare().await;
     let _ip = match a {
         Ok(resp) => {
-            let ip = resp.Answer.get(0).unwrap().data.replace("\"","");
-            ip
+            resp
         }
         Err(e) => {
             panic!("Fail to get ip :{}", e);
         }
     };
+    println!("{}", _ip);
 
     Ok(())
 }
